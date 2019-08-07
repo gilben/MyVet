309c52a30cd149b42252601e91a2895bf92dbef8
GILBERTO ANTONIO CASTRO FRANCO <gilbertocastro112521@correo.itm.edu.co>
2019-08-07 12:38:21 -0500

    Create Owner Class and DataContex Class

---
+++ MyVet.Web/Date/DataContext.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MyVet.Web.Date.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MyVet.Web.Date
+{
+    public class DataContext:DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options):base(options)
+        {
+
+        }
+
+        public DbSet<Owner> Owners { get; set; }
+    }
+}
